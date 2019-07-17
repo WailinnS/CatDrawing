61d3a296dd4a14103e4c851b864ba76ff7e688f6
Wailinn Saw <wailinnsaw@gmail.com>
2019-07-17 15:07:14 -0700

    Initial commit, cat head with ears

---
+++ myCat/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myCat
+{
+    public partial class Form1 : Form
+    {
+        Graphics gfx;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gfx = CreateGraphics();
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            Point pos = this.PointToClient(Cursor.Position);
+            this.Text = $"X: {pos.X} Y: {pos.Y}"; 
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //head & face
+            gfx.FillEllipse(Brushes.SteelBlue, 175, 75, 100, 100);
+            gfx.DrawArc(Pens.Black, 195, 111, 25, 25, 220, 100);
+            gfx.DrawArc(Pens.Black, 230, 111, 25, 25, 220, 100);
+            Point[] pnt = new Point[] 
+            {
+                new Point(224,127),
+                new Point(220,123),
+                new Point(228,123)
+            };
+            gfx.FillPolygon(Brushes.Pink, pnt);
+
+            //right ear
+            GraphicsPath outsideRight = new GraphicsPath();
+            GraphicsPath insideRight = new GraphicsPath();
+            Point[] rightFur = new Point[]
+            {
+                new Point(246,80),
+                new Point(265,50),
+                new Point(281,31),
+                new Point(261,90)
+            };
+            Point[] rightNoFur = new Point[]
+            {
+                new Point(261,90),
+                new Point(281,31),
+                new Point(272,108)
+            };
+            outsideRight.AddPolygon(rightFur);
+            insideRight.AddPolygon(rightNoFur);
+            gfx.FillPath(Brushes.SteelBlue,outsideRight);
+            gfx.FillPath(Brushes.Pink, insideRight);
+
+            //Left ear
+            GraphicsPath outsideLeft = new GraphicsPath();
+            GraphicsPath insideLeft = new GraphicsPath();
+            Point[] leftFur = new Point[]
+            {
+                new Point(185,95),
+                new Point(172,91),
+                new Point(120,83),
+                new Point(179,110)
+            };
+            Point[] leftNoFur = new Point[]
+            {
+                new Point(120,83),
+                new Point(178,110),
+                new Point(179,111),
+                new Point(176,136),
+                new Point(164,113)
+            };
+            outsideLeft.AddPolygon(leftFur);
+            insideLeft.AddPolygon(leftNoFur);
+            gfx.FillPath(Brushes.SteelBlue, outsideLeft);
+            gfx.FillPath(Brushes.Pink, insideLeft);
+
+            
+        }
+    }
+}
